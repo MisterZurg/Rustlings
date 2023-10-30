66f93000eedc36683ca563bd4ad3ef92962d97fb
MisterZurg <misterzurg@gmail.com>
2023-10-30 19:48:03 +0300

    Functions n Varibales done

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -2,18 +2,18 @@
 //
 // Execute `rustlings hint if3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn animal_habitat(animal: &str) -> &'static str {
-    let identifier = if animal == "crab" {
-        1
-    } else if animal == "gopher" {
-        2.0
-    } else if animal == "snake" {
-        3
-    } else {
-        "Unknown"
-    };
+    let identifier: i8 = if animal == "crab" {
+            1
+        } else if animal == "gopher" {
+            2
+        } else if animal == "snake" {
+            3
+        } else {
+            -1
+        };
 
     // DO NOT CHANGE THIS STATEMENT BELOW
     let habitat = if identifier == 1 {
