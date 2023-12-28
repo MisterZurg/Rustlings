69035c2420bf9fb3bc22620879b41e627147718b
MisterZurg <misterzurg@gmail.com>
2023-12-29 00:44:20 +0300

    Done hashmaps

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -39,6 +39,24 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let t_1_modify = scores.entry(team_1_name).or_insert(
+            Team{
+                goals_scored: 0, 
+                goals_conceded: 0,
+            }
+        );
+        (*t_1_modify).goals_scored += team_1_score;
+        (*t_1_modify).goals_conceded += team_2_score;
+
+
+        let t_2_modify = scores.entry(team_2_name).or_insert(
+            Team{
+                goals_scored: 0, 
+                goals_conceded: 0,
+            }
+        );
+        (*t_2_modify).goals_scored += team_2_score;
+        (*t_2_modify).goals_conceded += team_1_score;
     }
     scores
 }
