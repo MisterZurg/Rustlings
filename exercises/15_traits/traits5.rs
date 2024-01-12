5c15cb1c64246422357e0c341eabf1d956725c4c
MisterZurg <misterzurg@gmail.com>
2024-01-12 22:35:28 +0300

    Done traits

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
@@ -30,7 +30,11 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+// Solution 1: Multiple Trait as Parameter
+// fn some_func(item: (impl SomeTrait + OtherTrait)) -> bool {
+
+// Solution 2: Multiple Trait Bounds with the + Syntax
+fn some_func<T: SomeTrait + OtherTrait>(item: T) -> bool {
     item.some_function() && item.other_function()
 }
 
