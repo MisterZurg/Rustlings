0907273a5b5d307b84b61d3b573909f7eec52a39
MisterZurg <misterzurg@gmail.com>
2024-01-19 00:32:51 +0300

    Done Tests

--- exercises/17_tests/tests1.rs
+++ exercises/17_tests/tests1.rs
@@ -10,12 +10,15 @@
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        // Make the test pass!
+        assert!(true);
+        // Make the test fail!
+        // assert!(1337 == 228);
     }
 }
